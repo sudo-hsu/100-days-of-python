2f1e1df0d9a7bc5385271e3f8f18b41704090280
sudo-hsu <ehsu415@gmail.com>
2021-12-18 19:11:15 -0800

    pomodoro finish

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,39 +10,89 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
-def reset_clicked():
-    pass
+def reset_timer():
+    window.after_cancel(timer)
+    #timer_text 00:00
+    canvas.itemconfig(timer_text, text="00:00")
+    #title "Timer"
+    title.config(text="Timer")
+    #reset checkmarks
+    checkmarks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-def start_clicked():
-    pass
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it's the 1st/3rd/5th/7th rep:
+    if reps % 2 == 1:
+        countdown(work_sec)
+        title.config(text="Work", fg=GREEN)
+    # If it's the 8th rep:
+    elif reps % 8 == 0:
+        countdown(long_break_sec)
+        title.config(text="Break", fg=RED)
+    # If it's the 2nd/4th/6th rep:
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title.config(text="Break", fg=PINK)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        checkmarks.config(text=marks)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
 
 title = Label(text="Timer", font=("Arial", 50, "bold"), fg=GREEN, bg=YELLOW)
 title.grid(column=1, row=0)
 
-checkmarks = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmarks = Label(fg=GREEN, bg=YELLOW)
 checkmarks.grid(column=1, row=3)
 
-start = Button(text="Start", command=start_clicked, highlightbackground=YELLOW)
+start = Button(text="Start", command=start_timer, highlightbackground=YELLOW)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", command=reset_clicked, highlightbackground=YELLOW)
+reset = Button(text="Reset", command=reset_timer, highlightbackground=YELLOW)
 reset.grid(column=2, row=2)
 
 
