b0ab28734f7b10884dd7bbe7367e698e3eb45918
sudo-hsu <ehsu415@gmail.com>
2022-01-19 23:39:47 -0800

    day 32 progress
    completed challenge 1

--- day-32-send-email-dates/main.py
+++ day-32-send-email-dates/main.py
@@ -1,21 +1,31 @@
 import smtplib
 import secrets
 import datetime as dt
+import random
 
-# gmail = "test.sudohsu@gmail.com"
-# yahoo = "test.sudohsu@yahoo.com"
-# gmail_password = secrets.GMAIL_PASSWORD
-# yahoo_password = secrets.YAHOO_APP_PASSWORD
-# smtp_gmail = "smtp.gmail.com"
-# smtp_yahoo = "smtp.mail.yahoo.com"
-#
-# with smtplib.SMTP(smtp_gmail, port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=gmail, password=gmail_password)
-#     connection.sendmail(from_addr=gmail,
-#                         to_addrs=yahoo,
-#                         msg="Subject:Hello\n\nThis is the body of the email"
-#                         )
+gmail = "test.sudohsu@gmail.com"
+yahoo = "test.sudohsu@yahoo.com"
+gmail_password = secrets.GMAIL_PASSWORD
+#yahoo_password = secrets.YAHOO_APP_PASSWORD
+smtp_gmail = "smtp.gmail.com"
+#smtp_yahoo = "smtp.mail.yahoo.com"
 
-anniversary_date = dt.datetime(year=2019, month=4, day=6, hour=16, minute=30)
-print(anniversary_date)
+current_dt = dt.datetime.now()
+day_of_week = current_dt.weekday()
+
+with open("quotes.txt", "r") as data:
+    quotes_list = data.read().splitlines()
+
+# If it's a Monday, grab a random inspirational quote.
+if day_of_week == 0:
+    random_quote = random.choice(quotes_list)
+    with smtplib.SMTP(smtp_gmail, port=587) as connection:
+        connection.starttls()
+        connection.login(user=gmail, password=gmail_password)
+        connection.sendmail(from_addr=gmail,
+                            to_addrs=yahoo,
+                            msg=f"Subject:Time for your inspirational Monday quote!\n\n{random_quote}"
+                            )
+
+
+
